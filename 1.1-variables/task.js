993a8cd76444b175227d9a9fd6701ad77c821797
Alexandr Avakyan <avakjan@mail.ru>
2020-12-16 16:43:11 +0300

    Made task.js

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -6,22 +6,29 @@
  */
 
 function averageMark(a, g, p) {
-	
-    // реализуйте здесь задание №1
-    
-	return // ваш ответ;
+	// let algebra = a;
+	// let geography = g;
+	// let physics = p;
+
+	//let value = (a + g + p) / 3;
+     
+	return (a + g + p) / 3;
 }
 
 function sayHello(userName) {
-	
-    // реализуйте здесь задание №2
-    
-	return // ваш ответ;
+
+	//let myName = userName;
+
+	return `Привет, мир! Меня зовут ${userName}.`;
 }
 
 function calculateFormula() {
-    
-	// реализуйте здесь задание №3
-    
-    return // ваш ответ;
+
+	let x = 2;
+	let y = 22;
+	let z = 0;
+
+	let result = x * y + 5 * z + x - 1
+
+	return result;
 }
