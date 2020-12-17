7a3ef9c653cf49489c8b7bc35eb8b1522fd99871
Alexandr Avakyan <avakjan@mail.ru>
2020-12-18 00:22:40 +0300

    added hw2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,45 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+'use strict'
+
+function getResult(a, b, c) {
+    let x = [];
+    let D = b**2 - 4 * a * c;
+
+    if (D > 0) {
+        x.push((-b + Math.sqrt(D)) / 2 * a);
+        x.push((-b - Math.sqrt(D)) / 2 * a);
+    } else if (D === 0) {
+        x.push(-b / (2 * a));
+    } else {
+        x.push('Корней нет');
+    }
+
+    return x;
 }
 
-function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    // return averageMark;
+function getAverageMark(marks) {
+    let sum = 0;
+    
+    if (marks.length == '') {
+        return 0;
+    } else if (marks.length > 5) {
+        console.log('Больше допустимого');
+        marks.splice(5);
+    }
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+    }
+
+    return sum / marks.length;
 }
 
-function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    // return result;
+function askDrink(name,dateOfBirthday) {
+
+    let result = new Date().getFullYear() - dateOfBirthday.getFullYear();
+
+    if (result > 18) {
+        return `Не желаете ли олд-фэшн ${name}?`;
+    } else {
+        return `“Сожалею, ${name}, но я не могу вам продать алкоголь.`;
+    }
 }
