a32f8da153a46b523173a2d2f8d5074960dce1e6
Alexandr Avakyan <avakjan@mail.ru>
2020-12-20 20:38:49 +0300

    HW-1.3

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,28 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let bodyCredit = amount - contribution;
+
+    if (isNaN(percent)) {
+        return `Параметр <Процентная ставка> содержит неправильное значение ${percent}`;
+    } 
+
+    if (isNaN(contribution)) {
+        return `Параметр <Начальный взнос> содержит неправильное значение ${contribution}`;
+    }
+
+    if (isNaN(amount)) {
+        return `Параметр <Общая стоимость> содержит неправильное значение ${amount}`;
+    }
+
+    let month = (date.getFullYear() - new Date().getFullYear()) * 12 + date.getMonth() - new Date().getMonth();
+    let percentMonth = percent / 100 / 12;
+    let payment = bodyCredit * (percentMonth + percentMonth / (Math.pow(1 + percentMonth, month) - 1));
+    let totalAmount = (payment * month).toFixed(2);
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
-}
+    return `Привет, мир! Меня зовут ${name ? name : 'Аноним'}`;
+}вв
